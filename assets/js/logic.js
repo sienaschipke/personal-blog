35f4ed83f15f3d150757b942db5e8edd3082d2dd
Siena Schipke <sienaschipke@gmail.com>
2024-06-04 15:29:27 -0400

    blog.html build out

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,2 +1,5 @@
 // submit button should load blog posts page and add the new blog post
 // sun should change mode from light to dark
+    // background-color: black;
+    // color: white;
+// when you press back, page returns to home page
